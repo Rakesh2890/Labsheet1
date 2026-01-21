2a294f2452028056be283de2b0d1d85aaf1f4192
CDV057 <rakesh.raki.3678@gmail.com>
2026-01-21 12:09:14 +0530

    add numbers

---
+++ PU.CDV/src/main/java/simple_project/AddOfNum.java
@@ -0,0 +1,15 @@
+package simple_project;
+
+public class AddOfNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1 = 10;
+        int num2 = 20;
+        int sum = num1 + num2;
+
+        System.out.println(" sum of " + num1 + " and " + num2 + " is: " + sum);
+
+	}
+
+}
